f61d1fbf5e92766cfa7a8b9602b3f9df1bcb6276
32Nitesh <nk543729@gmail.com>
2022-11-09 10:40:35 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-
+require("@nomiclabs/hardhat-etherscan");
 /* global ethers task */
 require('@nomiclabs/hardhat-waffle')
 
@@ -25,5 +25,34 @@ module.exports = {
       enabled: true,
       runs: 200
     }
+  },
+  networks: {
+    goerli: {
+      url: "https://hardworking-wild-paper.ethereum-goerli.discover.quiknode.pro/e1693243a7682358a0536e263a812f3393814668/",
+      accounts: ["6169cb9b52222bf4c457e87e7c9c70bd0162aecad0e6aeccc857cd1c3eafd058"]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "RGC48BV4YX1NTFUN9S1XFJVHCU5DIZMUYN"
   }
 }
+
+//Diamond deployed: 0xacB2b26bed55f380d1A20826C2F809ac9f98710a
+//facetA deployed: 0x638944C91924B4912F63494de6926F4beb88C36F
+
+/////// Command //////
+
+//  npx hardhat compile
+
+//  npx hardhat run scripts/deploy.js --network goerli
+
+//   npx hardhat run scripts/deployFacet.js --network goerli
+
+//  npx hardhat verify --network goerli <deployFacet.js address>
+
+
+///******** How to clone the Diamond Contract *******/////
+
+// URL - https://www.quicknode.com/guides/smart-contract-development/the-diamond-standard-eip-2535-explained-part-2
